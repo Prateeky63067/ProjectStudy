0dbf00299bb5b5a5e5c7e1ea2db96ac3d1e80a0b
Prateeky63067 <shubhamy9451@gmail.com>
2024-02-05 00:31:34 +0530

    payment part done

--- src/components/core/Catalog/CourseSlider.jsx
+++ src/components/core/Catalog/CourseSlider.jsx
@@ -1,44 +1,40 @@
-import React from "react";
+import React from 'react'
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
-import { Autoplay, FreeMode, Navigation, Pagination } from "swiper";
+import {Swiper, SwiperSlide} from "swiper/react"
+import "swiper/css"
+import "swiper/css/free-mode"
+import "swiper/css/pagination"
+import { Autoplay,FreeMode,Navigation, Pagination}  from 'swiper'
 
-import Course_Card from "./Course_Card";
+import Course_Card from './Course_Card'
 
-const CourseSlider = ({ Courses }) => {
+const CourseSlider = ({Courses}) => {
   return (
     <>
       {Courses?.length ? (
         <Swiper
           slidesPerView={1}
+          spaceBetween={25}
           loop={true}
-          spaceBetween={200}
-          pagination={true}
-          modules={[Autoplay, Pagination, Navigation]}
-          className="mySwiper"
-          autoplay={{
-            delay: 1000,
-            disableOnInteraction: false,
-          }}
-          navigation={true}
+          modules={[FreeMode, Pagination]}
           breakpoints={{
-            1024: { slidesPerView: 3 },
+            1024: {
+              slidesPerView: 3,
+            },
           }}
+          className="max-h-[30rem]"
         >
-          {Courses?.map((course, index) => (
-            <SwiperSlide key={index}>
+          {Courses?.map((course, i) => (
+            <SwiperSlide key={i}>
               <Course_Card course={course} Height={"h-[250px]"} />
             </SwiperSlide>
           ))}
         </Swiper>
       ) : (
-        <p>No Course Found</p>
+        <p className="text-xl text-richblack-5">No Course Found</p>
       )}
     </>
-  );
-};
+  )
+}
 
-export default CourseSlider;
+export default CourseSlider
