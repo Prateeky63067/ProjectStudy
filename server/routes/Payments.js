0acc13e17b03cf05e6acf087fcea011c69a1b4ae
Prateeky63067 <shubhamy9451@gmail.com>
2024-02-07 03:20:17 +0530

    payement error-solve

--- server/routes/Payments.js
+++ server/routes/Payments.js
@@ -6,7 +6,7 @@ const {
   // verifySignature,
   verifyPayment,
   sendPaymentSuccessEmail,
-} = require("../controllers/payments")
+} = require("../controllers/Payments")
 const { auth, isInstructor, isStudent, isAdmin } = require("../middleware/auth")
 router.post("/capturePayment", auth, isStudent, capturePayment)
 router.post("/verifyPayment", auth, isStudent, verifyPayment)
