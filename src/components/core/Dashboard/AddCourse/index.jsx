0dde3a3884171dbbf61e378b8ff1bd27d2cf44fc
Prateeky63067 <shubhamy9451@gmail.com>
2024-02-17 22:16:29 +0530

    add course responsive

--- src/components/core/Dashboard/AddCourse/index.jsx
+++ src/components/core/Dashboard/AddCourse/index.jsx
@@ -3,8 +3,8 @@ import RenderSteps from "./RenderSteps"
 export default function AddCourse() {
   return (
     <>
-      <div className="flex w-full items-start gap-x-6">
-        <div className="flex flex-1 flex-col">
+      <div className="flex w-full items-start gap-x-6 ">
+        <div className="flex flex-1 flex-col  overflow-hidden ">
           <h1 className="mb-14 text-3xl font-medium text-richblack-5">
             Add Course
           </h1>
